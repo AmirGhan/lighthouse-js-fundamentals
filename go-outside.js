0ae54f12f058ce2ebb9f4f1914d42eead71b0d5b
Amir <amir.ghandeharioon@gmail.com>
2017-09-22 13:38:36 +0000

    Adds conditional exercises

---
+++ go-outside.js
@@ -0,0 +1,10 @@
+var tempreture = 16;
+
+if (tempreture < 0){
+	console.log("Make sure you pick out a scarf!");
+} else if (tempreture < 15) {
+	console.log("Short sleeves won' cut it!");
+} else {
+	console.log("Short sleeves are fine.");
+}
+console.log("Now you are ready to go outside!");
